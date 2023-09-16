c7a59abc9a5ba78afa7941d4373181bb20a7cebe
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-09-16 17:57:57 +0500

    more for sproduct

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ if (closeBtn) {
         navbar.classList.remove('active');
     })
 }
+
+// FOR SINGLE PRODUCT PAGE
+const MainImg = document.getElementById('MainImg');
+const smallImg = document.getElementsByClassName('small-img');
+
+for (let i = 0; i < smallImg.length; i++) {
+    smallImg[i].onclick = function () {
+        MainImg.src = smallImg[i].src
+    }
+}
